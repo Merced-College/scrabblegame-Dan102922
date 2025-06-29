c0cbd7ef59cc72508b720729afb88de3f0747ba3
Dan102922 <motad8940@gmail.com>
2025-06-29 11:28:16 -0700

    word class work in progress

--- Word.java
+++ Word.java
@@ -1,1 +1,11 @@
-//hi
+public class Word  implements Comparable<Word> {
+  private String word;
+
+  public Word(String w) {
+    this.word = w.toLowerCase();
+  }
+
+public String getWord() {
+  return word;
+  }
+}
